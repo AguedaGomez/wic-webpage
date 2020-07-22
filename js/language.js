17fb301619f8634b33c5b36452663722c1194dee
Agueda G <gmz.cambronero@gmail.com>
2020-07-22 11:01:48 +0200

    Update text

--- js/language.js
+++ js/language.js
@@ -10,59 +10,59 @@ var language = {
         video_presentation: "Watch Project Presentation",
         action_pretitle: "Our Action",
         action_title: "Why Women in <span>Copernicus?</span> <br> This is our mission",
-        action_p:"",
-        positions_title:"",
-        positions_p:"",
-        opportunities_title:"",
-        opportunities_p:"",
-        inspiration_title: "",
-        inspitarion_p: "",
-        survey_title:"",
-        survey_p:"",
-        survey_button:"",
-        funding_pretitle:"",
-        funding_title:"",
-        funding_p:"",
+        action_p: "Women are part of the Copernicus experience. They are not always visible but they are present in the production flow of the Copernicus / Earth Observation / Geoinformation (Copernicus/EO/GI) domains. The professions related to Copernicus/EO/GI domains are very varied: physicist, geographer, computer scientist, environmentalist, geomatician, oceanographer, urban planner ; as a researcher, a civil servant, a consultant, an entrepreneur, etc. Women still occupy few of these positions with a strong technical component. <br> The \"Women in Copernicus\" projects wants to give a voice and a face to the women who work with Copernicus, wether they are experts in the analysis of satellite data or users of Copernicus services in various fields of application.The project aims to identify opportunities and obstacles in the paths of these women, and also to inspire girls and women by bringing Women in Copernicus to the forefront of Copernicus.",
+        positions_title: "Positions",
+        positions_p: "Few positions with strong technical component are filled by women",
+        opportunities_title: "Opportunities",
+        opportunities_p: "We aim to identify opportunities and obstacles",
+        inspiration_title: "Inspiration",
+        inspitarion_p: "We want to inspire and encourage girls / women to study and apply for these positions",
+        survey_title: "Are you Woman working in Copernicus?",
+        survey_p: "This survey addresses European Women working (or having worked) with Copernicus data or in the Copernicus world (please just answer in that case). There are 6 sections (demographics, background, barriers, facilitators, education choices and final). This should take 15 minutes to answer.<br> Thank you for your time and involvement",
+        survey_button: "Fill in the survey",
+        funding_pretitle: "Who we are",
+        funding_title: "Funding and <span>Partners.</span>",
+        funding_p: "This project is funded by CoRdiNet (www.Cordinet.net), supported by NEREUS (Network of European Regions Using Space Technologies www.nereus-regions.eu), EURISY (European association of space agencies, www.eurisy.org) and EARSC (European Association of Remote Sensing Companies, www.earsc.org), and is led by BreTel (Brittany Remote Sensing Group, France), SPW (Public Service of Wallonia, Belgium) and UJI (Universtity Jaume I, Spain). The University of Strasbourg (France) and Salzburg (Austria) are partners.",
 
     },
     es: {
         home_subtitle: "Mujeres en Copernicus (Women in Copernicus) identificará, recopilará, entenderá y le abrirá las puertas a las historias de mujeres en Copernicus en las regiones de Europa",
         home_button: "Más sobre nosotras",
         video_presentation: "Vídeo de presentación",
-        action_pretitle: "",
-        action_title: "Why Women in <span>Copernicus?</span> <br> This is our mission",
-        action_p:"",
-        positions_title:"",
-        positions_p:"",
-        opportunities_title:"",
-        opportunities_p:"",
-        inspiration_title: "",
-        inspitarion_p: "",
-        survey_title:"",
-        survey_p:"",
-        survey_button:"",
-        funding_pretitle:"",
-        funding_title:"",
-        funding_p:"",
+        action_pretitle: "Nuestra acción",
+        action_title: "¿Por qué mujeres en <span>Copernicus?</span> <br> Esta es nuestra misión",
+        action_p: "Aunque a veces permanezcan ocultas, las mujeres son parte del programa Copernicus, y están están presentes en el flujo su producción. Las profesiones relacionadas con Copernicus, Observación de la Tierra y/o Información Geográfica son muy variadas: profesionales de Física, Geografía, Informática, Ciencias del Medio Ambiente, Geomática, Oceanografía, … asi como personal investigador, funcionario... pasando por profesionales en consultoría o emprendedores. Gran parte de estas profesiones, tienen una fuerte componente técnica que parece carecer de atractivo para otras muchas mujeres. <br> \"Mujeres en Copernicus\" (Women in Copernicus) quiere dar voz y rostro a las mujeres que trabajan en el ámbito de Copernicus, ya sean expertas en el análisis de datos satelitales o usuarias de los servicios de Copernicus en diversos campos de aplicación. El proyecto tiene como objetivo identificar oportunidades y obstáculos en el camino de estas mujeres, y también inspirar a las niñas y mujeres capaces de estar a la vanguardia del programa Copernicus.",
+        positions_title: "Puestos",
+        positions_p: "Existen pocos puestos de trabajo con un fuerte componente técnico que son ocupados por mujeres",
+        opportunities_title: "Oportunidades",
+        opportunities_p: "Identificamos las oportunidades y obstáculos",
+        inspiration_title: "Inspiración",
+        inspitarion_p: "Queremos inspirar e incentivar a mujeres y niñas a que estudien y apliquen para esos puestros de trabajo",
+        survey_title: "",
+        survey_p: "",
+        survey_button: "",
+        funding_pretitle: "",
+        funding_title: "",
+        funding_p: "",
     },
     fr: {
         home_subtitle: "Mujeres en Copernicus (Women in Copernicus) identificará, recopilará y entenderá...",
         video_presentation: "",
         action_pretitle: "",
         action_title: "",
-        action_p:"",
-        positions_title:"",
-        positions_p:"",
-        opportunities_title:"",
-        opportunities_p:"",
+        action_p: "Les femmes participent activement à l’expérience Copernicus. Elles sont parfois peu visibles Les femmes participent activement à l’expérience Copernicus. Elles sont parfois peu visibles mais bien présentes tout au long de la chaîne de production de Copernicus / de l’Observation de la Terre (EO) / de la Géoinformation (GI) (Copernicus/EO/GI). Les métiers liés à ces domaines sont très variés : physicienne, géographe, informaticienne, environnementaliste, océanographe, géomaticienne, urbaniste, etc. ; à des postes de chercheuses, fonctionnaires, consultantes, entrepreneures, etc. Une grande partie de ces professions ont une forte composante technique, et paraissent peu attrayantes pour beaucoup de femmes.\'Les Femmes et Copernicus\' (Women in Copernicus) veut donner une voix et un visage aux femmes qui œuvrent pour Copernicus, qu’elles soient expertes dans l’analyse des données satellitaires ou utilisatrices de services Copernicus pour divers domaines d’applications.  Ce projet vise à identifier les opportunités et les barrières sur le chemin de ces femmes, et également d’inspirer les filles et les femmes capables d’être au premier plan du progamme Copernicus.",
+        positions_title: "",
+        positions_p: "",
+        opportunities_title: "",
+        opportunities_p: "",
         inspiration_title: "",
         inspitarion_p: "",
-        survey_title:"",
-        survey_p:"",
-        survey_button:"",
-        funding_pretitle:"",
-        funding_title:"",
-        funding_p:"",
+        survey_title: "",
+        survey_p: "Cette enquête s’adresse aux femmes européennes travaillant ou ayant travaillé avec des données Copernicus ou dans les domaines d’activité liés à Copernicus. L’enquête compte 6 sections (démographique, parcours et carrière, barrières, facilitateurs, choix d’éducation et conclusion) et 10 minutes suffisent pour y répondre. Merci pour votre temps et votre intérêt pour cette initiative. * le genre féminin est indépendant du sexe biologique",
+        survey_button: "",
+        funding_pretitle: "",
+        funding_title: "",
+        funding_p: "Ce projet est financé par CoRdiNet, soutenu par NEREUS (Network of European Regions Using Space Technologies), EURISY (European association of space agencies), EARSC (European Association of Remote Sensing Companies) et BELSPO (Service public de programmation Politique scientifique, Belgique). Il est porté par BreTel (Bretagne Télédétection, France), SPW (Service Public de Wallonnie, Belgique) et UJI (Universtity Jacques Ier, Espagne). L’Université de Strasbourg (France) et l’Université de Salzburg (Autriche) viennent en support.",
 
     },
     it: {
@@ -70,19 +70,19 @@ var language = {
         video_presentation: "",
         action_pretitle: "",
         action_title: "",
-        action_p:"",
-        positions_title:"",
-        positions_p:"",
-        opportunities_title:"",
-        opportunities_p:"",
+        action_p: "",
+        positions_title: "",
+        positions_p: "",
+        opportunities_title: "",
+        opportunities_p: "",
         inspiration_title: "",
         inspitarion_p: "",
-        survey_title:"",
-        survey_p:"",
-        survey_button:"",
-        funding_pretitle:"",
-        funding_title:"",
-        funding_p:"",
+        survey_title: "",
+        survey_p: "",
+        survey_button: "",
+        funding_pretitle: "",
+        funding_title: "",
+        funding_p: "",
 
     },
     el: {
@@ -90,21 +90,41 @@ var language = {
         video_presentation: "",
         action_pretitle: "",
         action_title: "",
-        action_p:"",
-        positions_title:"",
-        positions_p:"",
-        opportunities_title:"",
-        opportunities_p:"",
+        action_p: "",
+        positions_title: "",
+        positions_p: "",
+        opportunities_title: "",
+        opportunities_p: "",
         inspiration_title: "",
         inspitarion_p: "",
-        survey_title:"",
-        survey_p:"",
-        survey_button:"",
-        funding_pretitle:"",
-        funding_title:"",
-        funding_p:"",
+        survey_title: "",
+        survey_p: "",
+        survey_button: "",
+        funding_pretitle: "",
+        funding_title: "",
+        funding_p: "",
 
-    }
+    },
+    de: {
+        home_subtitle: "Mujeres en Copernicus (Women in Copernicus) identificará, recopilará y entenderá...",
+        video_presentation: "",
+        action_pretitle: "",
+        action_title: "",
+        action_p: "",
+        positions_title: "",
+        positions_p: "",
+        opportunities_title: "",
+        opportunities_p: "",
+        inspiration_title: "",
+        inspitarion_p: "",
+        survey_title: "",
+        survey_p: "",
+        survey_button: "",
+        funding_pretitle: "",
+        funding_title: "",
+        funding_p: "",
+
+    },
 }
 
 var subtitle = document.getElementById("home_subtitle");
@@ -126,7 +146,7 @@ var funding_pretitle = document.getElementById("funding_pretitle");
 var funding_title = document.getElementById("funding_title");
 var funding_p = document.getElementById("funding_p");
 let texts = [subtitle, home_button, video_presentation, action_pretitle, action_title, action_p, positions_title, positions_p, opportunities_title, opportunities_p, inspiration_title, inspitarion_p,
-survey_title, survey_p, survey_button, funding_pretitle, funding_title, funding_p];
+    survey_title, survey_p, survey_button, funding_pretitle, funding_title, funding_p];
 
 changeLanguague();
 chooseLanguage();
@@ -137,7 +157,7 @@ function chooseLanguage() {
         texts.forEach(element => {
             var language_text = "language." + location.hash.slice(1) + "." + element.id;
             console.log(language_text);
-            element.innerHTML= eval(language_text);
+            element.innerHTML = eval(language_text);
         });
     }
     document.body.scrollTop = 0;
@@ -148,8 +168,8 @@ function chooseLanguage() {
 
 function changeLanguague() {
     var lang = window.location.pathname.split("#")[1];
-    if (lang=="") {
+    if (lang == "") {
         var userLang = navigator.language || navigator.userLanguage;
-        location.hash = "#" + userLang.slice(0,2);
+        location.hash = "#" + userLang.slice(0, 2);
     }
 }
